6b341641b3bd478c9a20c77fab6df622b4ea3cea
Mariana <118685930+marianadiazcas89@users.noreply.github.com>
2023-11-15 02:21:34 +0100

    Colecta de las noticias en el objeto newSchema

--- apinews.js
+++ apinews.js
@@ -1,21 +1,50 @@
-// conexion a la API NEWS
+// NEWS API connection
 
-// class NewsArticle {
-//     constructor(title, description, source, publishedAt) {
-//       this.title = title;
-//       this.description = description;
-//       this.source = source;
-//       this.publishedAt = publishedAt;
-//     }
-//   }
+// Declare the newSchema object:
+// It has several properties and their types (strings and 1 object)
+const newSchema = {
+    title: '',
+    description: '',
+    source: {
+        id: '',
+        name: ''
+    },
+    publishedAt: ''
+};
 
+// getNews Function:
+// This function takes a URL as a parameter and returns the result of the fetch function, which is used to make an HTTP request.
+function getNews(url) {
+    return fetch(url);
+}
+
+// getWorldNews Async Function:
+// This function uses the await keyword to wait for the completion of the asynchronous fetch operation to get news data from the specified URL.
 async function getWorldNews() {
-    try{
-        const response = await fetch ('https://newsapi.org/v2/top-headlines?language=en&apiKey=c8bb7c8b03bf4b528b528265a036c9dd&pageSize=100')
+    try {
+        const response = await fetch('https://newsapi.org/v2/top-headlines?language=en&apiKey=c8bb7c8b03bf4b528b528265a036c9dd&pageSize=100');
+        // It then converts the response to JSON format and extracts the articles array into newsList.
         const data = await response.json();
         const newsList = data.articles;
-        //const newsObjects = 
-    }catch(error){
+
+        // Iterating Through newsList:
+        // It uses the forEach method to iterate over each item in the newsList array.
+        // For each news item, it updates the properties of the newSchema object with values from the current news item.
+        // If a value is falsy (like null or undefined), it uses an empty string as a default.
+        // It then logs the updated newSchema object, --> Keep in mind that this will log the same object multiple times, each time it is updated.
+        newsList.forEach(news => {
+            newSchema.title = news.title || '';
+            newSchema.description = news.description || '';
+            newSchema.source.id = news.source.id || '';
+            newSchema.source.name = news.source.name || '';
+            newSchema.publishedAt = news.publishedAt || '';
+
+            // Do something with newSchema (e.g., print it, send it to another function, etc.)
+            console.log(newSchema);
+        });
+
+    // Error Handling:
+    } catch (error) {
         console.error('Ocurrio un error al obtener las noticias:', error.message);
     }
 }
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 //Env manejador de variables de entorno (ubicacion antes de que se ocupe)
 require('dotenv').config()
 // Conectarse a MongoDB
+//1.Llamo las librerias para que corra Mongo
 const { MongoClient, ServerApiVersion } = require('mongodb');
 // Construir la base de datos de la URL donde me voy a conectar
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@test.disdl6x.mongodb.net/?retryWrites=true&w=majority`;
@@ -17,14 +18,18 @@ const client = new MongoClient(uri, {
   }
 });
 
+// Crear una funcion que se conecte a Mongo (tiene que ser una funcion async porque se conecta a un recurso externo que no controlamos)
+// async implica ue cada interaccion con el recurso externo tenga un await.
 async function run() {
   try {
     await client.connect();
-    
     console.log("Conexión exitosa a MongoDB");
+    // conexion con base de datos
     const db = client.db(dbName);
+    //conexion con la coleccion
     const usuariosCollection = db.collection(collection);
     const usuario = { nombre: 'Ejemplo', email: 'ejemplo@email.com' };
+    // CRUD -> Create  
     await usuariosCollection.insertOne(usuario)
 
   } finally {
@@ -33,3 +38,4 @@ async function run() {
   }
 }
 run().catch(console.dir);
+
